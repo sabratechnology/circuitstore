ddc198726e006ea0822bb8db3545615ef01d2ea6
Rajkumar Namdev <rajkumar.namdev@hungama.com>
2024-01-14 00:25:08 +0530

    updated common navbar data api

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,7 @@
 const { check, validationResult: expressValidationResult } = require('express-validator');
 const Home = require('../models/Home');
+const User = require('../models/Users');
+
 
 // Validation middleware
 const validate = (req, res, next) => {
@@ -31,8 +33,17 @@ exports.getNavBarData = [
   validate,
   async (req, res) => {
     try {
-      const fData = await Home.navBarData(req.body);  
-      res.status(200).json({status: 200, code: true, message: 'success',data: fData });
+      const fData = await Home.navBarData(req.body); 
+      const userCartData = await User.userCartDataByUserId(req.body); 
+      const userWishListCount = await Home.getwishlistCount(req.body); 
+      const userProfileData = await User.userProfileDataById(req.body);
+      const wishlistCount = userWishListCount.wishlist_count ?? 0;
+      const cartData = userCartData.cart_data ? userCartData.cart_data : [];
+      const subTotalCartData = userCartData.sub_total ? userCartData.sub_total : 0;
+      const userName = userProfileData && userProfileData.length > 0 ? userProfileData[0].user_name : '';
+      const cartCount = cartData.length;
+
+      res.status(200).json({status: 200, code: true, message: 'success',cat_data:fData,cart_data:cartData,sub_total:subTotalCartData,cart_count:cartCount,wishlist_count:wishlistCount,user_profile_data:userName});
     } catch (error) {
       console.error('Error:', error);
       res.status(500).json({ status: 500, code: false, message: 'failure', error: error.message });
