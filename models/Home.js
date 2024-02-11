f3322cd4028fd9f93519d4acf385a52018202d20
Rajkumar Namdev <rajkumar.namdev@hungama.com>
2024-02-11 15:31:03 +0530

    bug fixed

--- models/Home.js
+++ models/Home.js
@@ -106,7 +106,7 @@ class Home {
           pr.featured = '1' 
           AND pr.status = '1' 
           AND pr.product_status = '1' 
-          AND inventory.used_status = '1',
+          AND inventory.used_status = '1'
           AND inventory.qty > '1'
           AND pr.qty > '1' 
       GROUP BY pr.product_id
