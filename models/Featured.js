c2dcc6b70f8d56f61d5a303db8f62269ad3eab49
Rajkumar Namdev <rajkumar.namdev@hungama.com>
2024-02-11 15:17:18 +0530

    stock qty updated

--- models/Featured.js
+++ models/Featured.js
@@ -74,6 +74,7 @@ class Featured {
           AND pr.status = '1' 
           AND pr.product_status = '1' 
           AND inventory.used_status = '1' 
+          AND inventory.qty > '1' 
       GROUP BY pr.product_id
       ${sorting}
       LIMIT ?, ?;`;
