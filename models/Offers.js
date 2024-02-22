8137c78d2f3336eeaca606b1a1b2523e12196890
Rajkumar Namdev <rajkumar.namdev@hungama.com>
2024-02-22 23:53:14 +0530

    offers code logic added

--- models/Offers.js
+++ models/Offers.js
@@ -1,48 +1,80 @@
 const db = require('../db');
 
 class Offers {
+   
+
     static async applyCouponCode(req) {
-        const userId = req.user_id;
-        const couponCode = req.coupon_code;
-        const cart_amount = req.cart_amount;
-        const currentDate = require('moment')().format('YYYY-MM-DD');
-        return new Promise(async (resolve, reject) => { // Mark the Promise callback function as async
-            const query = `SELECT * FROM offers WHERE CAST(end_date_time AS DATE) >= ? AND coupon_code = ? AND minimum_price <='?' AND maximum_price >= '?' AND status = 1`;
-            db.query(query, [currentDate, couponCode, cart_amount,cart_amount], async (error, results) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    try {
-                        if (results.length > 0) {
-                            const params = { user_id: userId, offer_type: results[0].offer_type, minimum_price: results[0].minimum_price, maximum_price: results[0].maximum_price, offer_amount: results[0].percentage };
-                            let offerCalculation = 0; // Declare offerCalculation outside the if conditions
-                    
-                            const offerUseStatus = await this.isOfferUsed(req);
-                    
-                            if (offerUseStatus) {
-                                if (results[0].offer_type == 2) { // Delivery offer
-                                    offerCalculation = await this.deliveryOfferCalculate(params);
-                                    if(offerCalculation){
-                                        offerCalculation = params;
+        return new Promise(async (resolve, reject) => {
+            try {
+                const userId = req.user_id;
+                const couponCode = req.coupon_code;
+                const cart_amount = req.cart_amount;
+                const currentDate = require('moment')().format('YYYY-MM-DD');
+    
+                const query = `SELECT * FROM offers 
+                               WHERE CAST(end_date_time AS DATE) >= ? 
+                               AND coupon_code = ? 
+                               AND minimum_price <= ? 
+                               AND status = 1`;
+    
+                db.query(query, [currentDate, couponCode, cart_amount], async (error, results) => {
+                    if (error) {
+                        reject(new Error(`Error executing database query: ${error.message}`));
+                    } else {
+                        try {
+                            if (results.length > 0) {
+                                const offerDetails = {
+                                    user_id: userId,
+                                    offer_type: results[0].offer_type,
+                                    minimum_price: results[0].minimum_price,
+                                    maximum_price: results[0].maximum_price,
+                                    offer_amount: results[0].percentage
+                                };
+    
+                                const isOfferUsed = await this.isOfferUsed(req);
+                                if (!isOfferUsed) {
+                                   
+                                    let offer_amounts;
+                                    if (offerDetails.offer_type == 2 && offerDetails.minimum_price == 0) {
+                                        offer_amounts = 0;
+                                    } else {
+                                        offer_amounts = offerDetails.offer_amount || 0;
+                                        if (offer_amounts !== 0) {
+                                            console.log(cart_amount)
+                                            offer_amounts = Math.round(cart_amount * (offer_amounts / 100));
+                                        }
                                     }
-                                } else if (results[0].offer_type == 3) { // Shopping offer
-                                    offerCalculation = await this.offerCalculate(params);
+                                    const bindParam = {
+                                        user_id: userId,
+                                        offer_type: offerDetails.offer_type,
+                                        offer_amount: offer_amounts
+                                    };
+                                    const offerCalculation = await this.deliveryOfferCalculate(bindParam);
+                                    if (offerCalculation) {
+                                        resolve(bindParam);
+                                    } else {
+                                        reject(new Error('Error in offer calculation.'));
+                                    }
+                                } else {
+                                    reject('Coupon code has already been used.');
                                 }
+                            } else {
+                                reject('No results found for the given coupon code.');
                             }
-                    
-                            resolve(offerCalculation);
-                        } else {
-                            reject('Coupon code has expired or is invalid.');
+                        } catch (error) {
+                            reject(new Error(`Error during offer processing: ${error.message}`));
                         }
-                    } catch (error) {
-                        reject(error);
                     }
-                    
-                }
-            });
-            
+                });
+            } catch (error) {
+                reject(new Error(`Unexpected error: ${error.message}`));
+            }
         });
     }
+    
+    
+    
+    
 
 
 
@@ -60,7 +92,7 @@ class Offers {
                     if (results.length > 0) {
                         reject('Coupon code Already Used.');
                     } else {
-                        resolve(true);
+                        resolve(false);
                     }
                 } catch (error) {
                     reject(error);
@@ -69,14 +101,9 @@ class Offers {
           });
         });
       }
-
-    
       static async deliveryOfferCalculate(req){
-
         var userId = req.user_id;
         var couponCode = req.coupon_code;
-        
-
         return new Promise((resolve, reject) => {
           const query = "SELECT * FROM order_data WHERE fk_user_id = ? AND has_coupon_code = 1 AND coupon_number = ? AND status =1;";
           db.query(query,[userId,couponCode], (error, results) => {
@@ -87,7 +114,6 @@ class Offers {
                     if (results.length > 0) {
                         reject('Coupon code Already Used.');
                     } else {
-
                         resolve(true);
                     }
                 } catch (error) {
@@ -97,6 +123,13 @@ class Offers {
           });
         });
       }
+
+
+      
+
+
+
+      
 
 }
 
